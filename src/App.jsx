d4de8170f326a20d5971bf5799251cef1a5364b8
arthurlch <lachat.arthur74@gmail.com>
2021-12-05 15:53:11 +0900

    fix: for react router component rendering is component={mycomponent} instead of router v6 element={mycomponent}

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./components/Layout";
-import { Routes, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <Layout>
-        <Routes>
-          <Route index path='/' element={<Home />} />
-        </Routes>
+        <Switch>
+          <Route index path='/' component={Home} />
+        </Switch>
       </Layout>
     </>
   );
--- src/components/Home/Project.jsx
+++ src/components/Home/Project.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, CardGroup } from "react-bootstrap";
 import "../../styles/Home/Project.css";
 
 const Project = () => {
@@ -9,12 +9,49 @@ const Project = () => {
           <h4 className='projects-title'>Things I do..</h4>
         </Col>
       </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-      </Row>
+      <CardGroup>
+        <Card>
+          <Card.Img variant='top' src='holder.js/100px160' />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant='top' src='holder.js/100px160' />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant='top' src='holder.js/100px160' />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
     </Container>
   );
 };
