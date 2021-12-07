446e94c027df0f01479462da3817e48fdc850e7f
arthurlch <lachat.arthur74@gmail.com>
2021-12-07 19:09:14 +0900

    feat: finish styling of project component

--- src/components/Home/Project.jsx
+++ src/components/Home/Project.jsx
@@ -1,23 +1,23 @@
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import "../../styles/Home/Project.css";
-import { GoMarkGithub, GoLinkExternal } from "react-icons/go";
-import { Railsbnb } from "../../data/Homepage/projectsData";
-// https://placeholder.com/
+import { GoMarkGithub, GoLinkExternal, GoArrowRight } from "react-icons/go";
+import { Railsbnb, PyMaze, RubyCRUD } from "../../data/Homepage/projectsData";
+
 const Project = () => {
   // Desconstructing Object here is useless since data are available locally.
   return (
     <Container fluid className='project'>
       <Row>
         <Col className='d-flex my-5 '>
-          <h4 className='projects-title'>Things I do..</h4>
+          <h2 className='projects-title'>Some of my latest work..</h2>
         </Col>
       </Row>
 
       <Container>
         <Row>
-          <Col>
+          <Col md={4} className='card-col'>
             <Card className=' card h-100'>
-              <Card.Img variant='top' src='https://i.imgur.com/hcrPsdJ.png' />
+              <Card.Img variant='top' src={Railsbnb.CardImg} />
               <Card.Body>
                 <Card.Title className='card-title'>
                   {Railsbnb.CardTitle}
@@ -33,61 +33,56 @@ const Project = () => {
                 <a href={Railsbnb.CardLink}>
                   <GoLinkExternal className='project-icon' />
                 </a>
-                <Button className='card-btn' size='sm' variant='outline-dark'>
-                  More
-                </Button>
               </Card.Footer>
             </Card>
           </Col>
 
-          <Col>
+          <Col md={4} className='card-col'>
             <Card className=' card h-100'>
-              <Card.Img variant='top' src='https://i.imgur.com/hcrPsdJ.png' />
+              <Card.Img variant='top' src={PyMaze.CardImg} />
               <Card.Body>
                 <Card.Title className='card-title'>
-                  {Railsbnb.CardTitle}
+                  {PyMaze.CardTitle}
                 </Card.Title>
                 <Card.Text className='card-description'>
-                  {Railsbnb.CardDescription}
+                  {PyMaze.CardDescription}
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <a href={Railsbnb.CardGithubLink} target='_blank'>
+                <a href={PyMaze.CardGithubLink} target='_blank'>
                   <GoMarkGithub className='project-icon' />
                 </a>
-                <a href={Railsbnb.CardLink}>
-                  <GoLinkExternal className='project-icon' />
-                </a>
-                <Button className='card-btn' size='sm' variant='outline-dark'>
-                  More
-                </Button>
               </Card.Footer>
             </Card>
           </Col>
 
-          <Col>
+          <Col md={4} className='card-col'>
             <Card className=' card h-100'>
-              <Card.Img variant='top' src='https://i.imgur.com/hcrPsdJ.png' />
+              <Card.Img variant='top' src={RubyCRUD.CardImg} />
               <Card.Body>
                 <Card.Title className='card-title'>
-                  {Railsbnb.CardTitle}
+                  {RubyCRUD.CardTitle}
                 </Card.Title>
                 <Card.Text className='card-description'>
-                  {Railsbnb.CardDescription}
+                  {RubyCRUD.CardDescription}
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <a href={Railsbnb.CardGithubLink} target='_blank'>
+                <a href={RubyCRUD.CardGithubLink} target='_blank'>
                   <GoMarkGithub className='project-icon' />
                 </a>
-                <a href={Railsbnb.CardLink}>
-                  <GoLinkExternal className='project-icon' />
-                </a>
-                <Button className='card-btn' size='sm' variant='outline-dark'>
-                  More
-                </Button>
               </Card.Footer>
             </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col className='project-more'>
+            <Button className='project-btn'>
+              Check more
+              <span>
+                <GoArrowRight className='project-more-arrow' />
+              </span>
+            </Button>
           </Col>
         </Row>
       </Container>
--- src/data/Homepage/projectsData.js
+++ src/data/Homepage/projectsData.js
@@ -4,4 +4,29 @@ export const Railsbnb = {
     "A full fledged Booking APP build with Ruby on Rails and React, features: Booking, Properties management, Payments via Stripe.",
   CardGithubLink: "https://github.com/arthurlch/RailsBnB",
   CardLink: "https://railsbnb1.herokuapp.com/",
+  CardImg: "https://i.imgur.com/E97GXBp.png",
+};
+
+export const PersonalWebsite = {
+  CardTitle: "My Website",
+  CardDescription:
+    " Made with React/Vite, contain my blog with Dev.to API and my portfolio",
+  CardGithubLink: "https://github.com/arthurlch/mypw",
+  CardImg: "https://i.imgur.com/CsQ03Q2.png",
+};
+
+export const PyMaze = {
+  CardTitle: "PyMaze",
+  CardDescription:
+    "Maze game made with Python3/PyGame. Object Oriented, Tiles, Colision, Inventory, Random Object spawning.",
+  CardGithubLink: "https://github.com/arthurlch/PYMaze",
+  CardImg: "https://i.imgur.com/TwzxEQt.png",
+};
+
+export const RubyCRUD = {
+  CardTitle: "RubyCRUD",
+  CardDescription:
+    "Twitter Clone with Ruby On Rails, has user login etc functionnalities, Tweet CRUD Operations.",
+  CardGithubLink: "https://github.com/arthurlch/PYMaze",
+  CardImg: "https://i.imgur.com/fEcZx1f.png",
 };
