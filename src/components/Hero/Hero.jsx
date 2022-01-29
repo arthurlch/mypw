87204aa405f09cf3ceb52149443354a10adf6b8b
arthurlch <lachat.arthur74@gmail.com>
2022-01-29 22:41:23 +0900

    feat; finished the main design for the homepage, btn effect and animation adnz index fixes are done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,36 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import ThreeJs from '../../Animated/Threejs';
-import './../../styles/Components/Hero.css';
+import './../../styles/Components/Hero.scss';
+import { BiRightArrow } from 'react-icons/bi';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
-  return <ThreeJs />;
+  return (
+    <div className='hero-wrapper'>
+      <div className='header-animation'>
+        <ThreeJs />
+      </div>
+      <Container className='container-header'>
+        <Row className='row-header'>
+          <Col>
+            <div className='header'>
+              <h1>
+                The true sign of intelligence is not knowledge but imagination
+              </h1>
+              <div className='btn-wrapper'>
+                <Button className='btn'>
+                  <a>Let's imagine together</a>
+                  <span className='header-btn-icon'>
+                    <BiRightArrow />
+                  </span>
+                </Button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Hero;
