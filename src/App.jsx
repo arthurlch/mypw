792b9513e9b98379224b6ab1be193f8d3990f5d3
arthurlch <lachat.arthur74@gmail.com>
2022-01-31 21:25:53 +0900

    feat: article component restyling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './components/Home/Home';
 import { About } from './components/About/About';
 import { Project } from './components/Project/Project';
 import { Contact } from './components/Contact/Contact';
+import { Blog } from './components/Blog/Blog';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route index path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/blog' element={<Blog />} />
           <Route path='/projects' element={<Project />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
